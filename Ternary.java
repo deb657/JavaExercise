1bf2a33977ba65a8aa797842bac16a01821e01e2
deb657 <devilalgodara98@gmail.com>
2023-02-02 18:50:20 +0530

    change

--- Ternary.java
+++ Ternary.java
@@ -5,6 +5,10 @@ class Ternary{
         int c=40;
         int res=(a<b)? a:b;
         System.out.println(res);
+        String result=(a<c) ? "a is less" : "c is less";
+        System.out.println(result);
+
         //this is a ternary program
+
     }
 }
