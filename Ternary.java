064d05a2968038515d7b5c98ab0a1ddf494e27d7
deb657 <devilalgodara98@gmail.com>
2023-02-02 18:40:16 +0530

    Java Exercise

---
+++ Ternary.java
@@ -0,0 +1,9 @@
+class Ternary{
+    public static void main(String args[]){
+        int a=10;
+        int b=30;
+        int c=40;
+        int res=(a<b)? a:b;
+        System.out.println(res);
+    }
+}
