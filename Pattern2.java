7050228c1b7bbb09ac3a4396f8bcff35fa996be8
deb657 <devilalgodara98@gmail.com>
2023-02-05 03:19:57 +0530

    changed the value of "n"

--- Pattern2.java
+++ Pattern2.java
@@ -3,7 +3,7 @@ public class Pattern2 {
         int n = 5;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || i==(n/2)   || j==0 || j==4){
+                if(i==0 || i==(n-1/2)   || j==0 || j==4){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
