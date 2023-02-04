82ff52be134e8435a1771c9b24bc484aa5b57a9d
deb657 <devilalgodara98@gmail.com>
2023-02-05 04:04:40 +0530

    "H" star pattern

--- Pattern4.java
+++ Pattern4.java
@@ -1,5 +1,17 @@
 class Pattern4{
-    public static void main(Stirng args[]){
-        
+    public static void main(String args[]){
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==(n-1)/2 || j==0 || j==n-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+
+            System.out.println();
+        }
         }
 }
