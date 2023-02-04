23f2ab0515cd77afb4266a1b2f18b1505fe334c9
deb657 <devilalgodara98@gmail.com>
2023-02-05 01:54:57 +0530

    Changed the if loop condition

--- Pattern1.java
+++ Pattern1.java
@@ -3,7 +3,7 @@ public class Pattern1 {
         int n=5;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || i==4 || j==0 || j==4){
+                if(i==0 || i==n-1 || j==0 || j==n-1){
                 System.out.print("*");
                 }else{
                     System.out.print(" ");
