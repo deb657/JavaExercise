613101f17d423ebf5961e3b83b33169dee33d098
deb657 <devilalgodara98@gmail.com>
2023-02-05 04:46:03 +0530

    complete "A" pattern

--- Pattern2.java
+++ Pattern2.java
@@ -4,9 +4,9 @@ public class Pattern2 {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==0 || i==(n-1)/2   || j==0 || j==n-1){
-                    System.out.print("*");
+                    System.out.print("* ");
                 }else{
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
